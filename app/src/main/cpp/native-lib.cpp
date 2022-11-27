10d6be10e42f80a1e0ef6a37dfb5a4fefcc7a8f5
21KM4 <kokimizu1@gmail.com>
2022-11-27 21:57:43 +0900

    None

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,27 @@
-#include <jni.h>
-#include <string>
-//
-//
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_wasa_eet23_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+#include "DxLib.h"
+#include "json.hpp"
+static const int SCREEN_WIDTH = 1080;
+static const int SCREEN_HEIGHT = 2340;
+
+int android_main() {
+
+    // 画面の解像度、カラービットの設定
+    SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32);
+    // 背景色の指定
+    SetBackgroundColor(64, 64, 64);
+    // DXライブラリの初期化
+    DxLib_Init();
+    // 描画先を裏画面に設定
+    SetDrawScreen(DX_SCREEN_BACK);
+
+    // ここで画像のロード、初期設定を行う
+
+    // 1秒間に60回繰り返される
+    while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
+
+    }
+
+    // DXライブラリ終了処理
+    DxLib_End();
+    return 0;
 }
